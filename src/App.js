80d30369b6b6c9a03247cd03603c3ad4ecab6e8d
Sorelul <sorinake_eu@yahoo.com>
2025-02-04 14:14:14 +0200

    Add Main and StartScreen components; refactor App to manage quiz state and loading status

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+    questions: [],
+    // 'loading' , 'error', 'ready', 'active', 'finished'
+    status: "loading",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataReceived":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready",
+            };
+        case "datafailed":
+            return {
+                ...state,
+                status: "error",
+            };
+        default:
+            return new Error("Invalid action type");
+    }
+}
 
 export default function App() {
+    const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+    const numQuestions = questions.length;
+
+    useEffect(() => {
+        fetch("http://localhost:8000/questions")
+            .then((res) => res.json())
+            .then((data) => dispatch({ type: "dataReceived", payload: data }))
+            .catch((err) => dispatch({ type: "datafailed" }));
+    }, []);
+
     return (
-        <div>
-            <DateCounter />
+        <div className="app">
+            <Header />
+            <Main>
+                {status === "loading" && <Loader />}
+                {status === "error" && <Error />}
+                {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+            </Main>
         </div>
     );
 }
