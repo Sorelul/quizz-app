fe192c6d04635417e3aaedc02107fd8dc87d8ca2
Sorelul <sorinake_eu@yahoo.com>
2025-02-04 22:33:09 +0200

    Add Progress component to display quiz progress and update App component to manage points and progress state

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
     questions: [],
@@ -56,9 +57,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-    const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
+    const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
 
     const numQuestions = questions.length;
+    const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0);
 
     useEffect(() => {
         fetch("http://localhost:8000/questions")
@@ -76,6 +78,13 @@ export default function App() {
                 {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
                 {status === "active" && (
                     <>
+                        <Progress
+                            index={index}
+                            numQuestions={numQuestions}
+                            points={points}
+                            maxPossiblePoints={maxPossiblePoints}
+                            answer={answer}
+                        />
                         <Question question={questions.at(index)} dispatch={dispatch} answer={answer} />
                         <NextButton dispatch={dispatch} answer={answer} />
                     </>
